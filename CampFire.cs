f6a429c87cf144c0af39023c9ee6c54518647f6b
Timofey.Sychev@urfu.me <Timofey.Sychev@urfu.me>
2021-04-07 13:23:04 +0500

    first commit

---
+++ CampFire.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newGame
+{
+    public class CampFire
+    {
+        private static int health { get; set; }
+        private int locationX;
+        private int locationY;
+        private static bool flame { get; set; }
+
+        public static void DeterminStatus(int health)
+        {
+            if (health <= 0)
+            {
+                flame = false;
+            }
+            else
+            {
+                flame = true;
+            }
+        }
+    }
+}
---
+++ Field.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newGame
+{
+    class Field
+    {
+        private static bool tree { get; set; }
+
+    }
+}
